413df40cf5ca5ff0c0758b30b91f71c8c3efd2cc
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-08-18 04:35:43 +0300

    as

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('play', function (Request $request) {
     if ($moviemodel == null) {
         return die('Movie not found');
     }
-    $newUrl = url('storage/' . $moviemodel->url);
+    $newUrl = url('storage/' . $moviemodel->new_server_path); 
     //html player for new and old links
     $html = '<video width="320" height="240" controls>
                 <source src="' . $moviemodel->url . '" type="video/mp4">
